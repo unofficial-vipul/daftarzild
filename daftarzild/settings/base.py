26a180e7213a0aa23a0d9be1e503902cbf7a9fe5
Vipul <vipul.sharma029@gmail.com>
2016-08-28 17:22:22 +0530

    static changes 2

--- daftarzild/settings/base.py
+++ daftarzild/settings/base.py
@@ -109,9 +109,9 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
 ]
 
-STATIC_ROOT = os.path.dirname(BASE_DIR), "static_cdn"
+STATIC_ROOT = os.path.join(BASE_DIR, "static_cdn")
 
 MEDIA_URL = "/media/"
-MEDIA_ROOT = os.path.dirname(BASE_DIR), "media_cdn"
+MEDIA_ROOT = os.path.join(BASE_DIR, "media_cdn")
 
 STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
