f4b03724785709bf3fdbdda6b3f9d5c1f08aa60a
Vipul <vipul.sharma029@gmail.com>
2016-08-28 18:18:09 +0530

    changes 42

--- daftarzild/settings/base.py
+++ daftarzild/settings/base.py
@@ -106,7 +106,7 @@ STATIC_URL = '/static/'
 
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static"),
+    os.path.join(BASE_DIR, 'static', 'static_dirs'),
 ]
 
 #STATIC_ROOT = '/Users/jmitch/desktop/lwc/src/static/static_root/'
