f6203a8a5391105ae03ff86922168844efc30546
Vipul <vipul.sharma029@gmail.com>
2016-08-28 22:32:10 +0530

    trying a jugaad fix static files

--- daftarzild/settings/base.py
+++ daftarzild/settings/base.py
@@ -110,7 +110,7 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
 ]
 
-STATIC_ROOT = os.path.join(BASE_DIR, "../static_cdn")
+STATIC_ROOT = os.path.join(BASE_DIR, "static_cdn")
 
 MEDIA_URL = "/media/"
 MEDIA_ROOT = os.path.join(BASE_DIR, "media_cdn")
