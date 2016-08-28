7c07f1c7ae05a1c92be7131d8270c3cec65a1fe6
Vipul <vipul.sharma029@gmail.com>
2016-08-28 22:18:59 +0530

    fixing static files

--- daftarzild/settings/base.py
+++ daftarzild/settings/base.py
@@ -101,6 +101,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
 STATIC_URL = '/static/'
 
