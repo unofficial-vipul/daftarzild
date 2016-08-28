51fadfab49c3e85f13ff46295eb51845eef58c3a
Vipul <vipul.sharma029@gmail.com>
2016-08-28 22:50:39 +0530

    ek aur jugad net se

--- daftarzild/urls.py
+++ daftarzild/urls.py
@@ -26,3 +26,8 @@ urlpatterns = [
 if settings.DEBUG:
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+if not settings.DEBUG:
+    urlpatterns += patterns('',
+        (r'^static/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.STATIC_ROOT}),
+    )
